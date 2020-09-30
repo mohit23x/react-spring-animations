98740610999fb004044ecf2c35192bdfa29ed906
Mohit <mohit@symboinsurance.com>
2020-09-30 22:52:20 +0530

    added spring config presets

---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+export const wobbly = {
+  tension: 180,
+  friction: 12,
+};
+
+export const gentle = {
+  tension: 120,
+  friction: 14,
+};
+
+export const stiff = {
+  tension: 210,
+  friction: 20,
+};
+
+export const slow = {
+  tension: 280,
+  friction: 60,
+};
